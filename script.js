ad91165d7b9580621920f5015051403a7ca41444
Aim <teodziv@gmail.com>
2026-05-22 15:10:11 +0200

    base
    struttura html, primo js e css

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function card1() {
+    cardone.classList.toggle("flipped"); 
+    console.log("flipped")
+}
+
+function card2() {
+    cardtwo.classList.toggle("flipped"); 
+    console.log("flipped")
+}
